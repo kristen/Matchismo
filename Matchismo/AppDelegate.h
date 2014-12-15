cb1988a6b643ad683bf14d79e3aa27a11538cfbe
Kristen Sundquist <kristen@sigfig.com>
2014-12-15 13:21:12 -0800

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Kristen on 12/15/14.
+//  Copyright (c) 2014 Kristen Sundquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Kristen on 12/15/14.
+//  Copyright (c) 2014 Kristen Sundquist. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
